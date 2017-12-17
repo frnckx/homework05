d8c8d62d78ba2d5858783a5f2f37887bf3b11558
frnckx <jakab.fruzsi@gmail.com>
2017-12-17 21:50:40 +0100

    #4

--- src/hu/homework05/jf/Main.java
+++ src/hu/homework05/jf/Main.java
@@ -10,6 +10,16 @@ public class Main {
         //3. feladat teszt
         int[][] matrix = matrixOperation.initMatrixWithRandomNumbers(5, 6, 5, 9);
         matrixOperation.printMatrix(matrix);
+        Sort sort = new Sort();
+//4. A teszteléshez 2 tömböt is létrehoztam, az egyik már eleve sorban van, a másik nem
+        int[] array = {1, 2, 3, 4, 5};
+        int[] array2 = {1, 8, 3, 7, 5, 3, 2, 1};
+        sort.bubbleSort(array);
+        sort.bubbleSort(array2);
+        for (int i = 0; i < array2.length; i++) {
 
+            System.out.print(array2[i]+" ");
+
+        }
     }
 }
--- src/hu/homework05/jf/Sort.java
+++ src/hu/homework05/jf/Sort.java
@@ -1,9 +1,11 @@
 package hu.homework05.jf;
 
 public class Sort implements ISort {
-
+//4.feladat: a pseudocode alapján optimalizáltam a kódot, a teszteléshez kiírattam vele a cserék számát, illetve
+// ha nem volt szükség cserére, akkor hogy breakkel megszakadt a futás
     @Override
     public void bubbleSort(int[] array) {
+        int swaps = 0;
         for (int i = array.length - 1; i >= 1; i--) {
 
             for (int j = 0; j <= i - 1; j++) {
@@ -12,8 +14,15 @@ public class Sort implements ISort {
                     int swap = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = swap;
-
+                    swaps++;
+                    System.out.println("swaps: "+swaps);
                 }
+
+            }
+            if (swaps == 0){
+                System.out.println("Break");
+                break;
+
             }
 
         }
